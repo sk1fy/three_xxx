8186e4beb1de99cd74cb82f2bba5e6fa83d08b3d
sk1fy <50354515+sk1fy@users.noreply.github.com>
2021-02-15 04:35:51 +0800

    card fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,31 @@ document.querySelector(".header-menu").onclick = function () {
     document.querySelector("html").classList.toggle("overflow");
   }
 };
+
+function showModalOverlay(el) {
+  document.querySelector(el).classList.add('show');
+}
+
+function closeModalOverlay(el) {
+  document.querySelector(el).classList.remove('show')
+}
+
+function dataSetInfo(e) {
+  const data = e.dataset.product;
+  const element = document.querySelector(`#product-${data}`);
+  element.classList.remove('closed')
+  element.classList.toggle('fixed')
+  setTimeout(function(){
+    element.classList.toggle('anim');
+  },300);
+  document.querySelector('html').classList.toggle('overflow-hidden')
+  if (!element.className.match('fixed')) {
+    element.classList.add('closed')
+    e.dataset.closed = true
+  }
+}
+
+// document.querySelector('.search').onclick = function () {
+//   document.querySelector(".search_wrap").classList.toggle("collapse-search");
+// }
+
