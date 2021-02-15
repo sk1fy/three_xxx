d8ab9edc94bc05ec92429d40627b73853d692f80
sk1fy <streyking@mail.ru>
2021-02-15 18:03:48 +0800

    pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,33 @@
+document.querySelector("body").onload = function () {
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector(".mini-cart").style.opacity = "0";
+    } else {
+      document.querySelector(".mini-cart").style.opacity = "1";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+};
+
 document.querySelector(".header-menu").onclick = function () {
   document.querySelector(".header-list-menu").classList.toggle("visible");
+  const e = document.querySelector(".header-menu");
+  const menuOpen = e.querySelectorAll("img")[0];
+  const menuClose = e.querySelectorAll("img")[1];
+  if (menuOpen.className.match("active")) {
+    menuOpen.classList.remove("active");
+    menuClose.classList.add("active");
+  } else if (menuClose.className.match("active")) {
+    menuClose.classList.remove("active");
+    menuOpen.classList.add("active");
+  }
+
   if (window.innerWidth <= 990) {
     document.querySelector("html").classList.toggle("overflow-hidden");
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 };
 
@@ -27,7 +53,3 @@ function dataSetInfo(e) {
     e.dataset.closed = true;
   }
 }
-
-// document.querySelector('.search').onclick = function () {
-//   document.querySelector(".search_wrap").classList.toggle("collapse-search");
-// }
